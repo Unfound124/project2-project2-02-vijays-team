634da271e809b3c966ca3a84dc37a4b4d85382b8
Unfound124 <vduggirala@csu.fullerton.edu>
2018-10-07 01:54:12 -0700

    Finished Webpage.h

--- Webpage.h
+++ Webpage.h
@@ -6,7 +6,7 @@ using namespace std;
 class Webpage {
 public:
     Webpage();
-    Webpage(const string& webpageUrl, const time_t& timeVisited);
+    Webpage(const string&, const time_t&);
     string getUrl();
     time_t getTime();
 
@@ -14,3 +14,21 @@ private:
     string url;
     time_t time;
 };
+
+Webpage::Webpage(){
+    url = "#";
+    time = time_t(0000000000);
+}
+
+Webpage::Webpage(const string& webpageUrl, const time_t& timeVisited){
+    url = webpageUrl;
+    time = timeVisited;
+}
+
+string Webpage::getUrl(){
+    return url;
+}
+
+time_t Webpage::getTime(){
+    return time;
+}
