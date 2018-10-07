5e8fded0043ec0f6aea3b5ea0f7cb4c69be1b8d4
Unfound124 <vduggirala@csu.fullerton.edu>
2018-10-07 12:04:51 -0700

    Updated BrowserHistory.h

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -28,7 +28,46 @@ private:
     list<Webpage> sitesVisited;
     int numSites;
 };
+BrowserHistory::BrowserHistory(){
+    numSites = 0;
+    navPos = navHistory.end();
+}
+string BrowserHistory::back(){
+    if (navHistory.size()<=1)
+        throw invalid_argument("Can't go back. History is empty");
+    --navPos;
+       
+    return navPos->getUrl();        
+}
+string BrowserHistory::forward(){
+    if(navHistory.end() == navPos)
+        throw invalid_argument("Can't go forward. Forward History is empty.");
+    navPos++;
+    string _url = getUrl();
+    return _url;
+}
+size_t BrowserHistory::getNavSize(){
+    size_t temp = navHistory.size();
+    
+    return temp;
+}
+string BrowserHistory::getUrl(){
+    if(navHistory.empty())
+    {
+        throw invalid_argument("History Empty");
+    }
+    return navPos->getUrl();
+}
 
+list<Webpage> BrowserHistory::getSitesVisited(){
+    return sitesVisited;
+}
+void BrowserHistory::visitSite(Webpage newSite){
+    navHistory.push_back(newSite);
+    sitesVisited.push_back(newSite);
+    numSites++;
+    //navPos = navHistory.end();
+}
 void BrowserHistory::readHistory(string fileName) {
     string temp;
     int newTime;
